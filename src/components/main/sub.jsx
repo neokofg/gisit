81b9e238cf197fe5db1e4c62699b038608ce81d7
Victor <72002526+victor-ivanov-ivt20-2@users.noreply.github.com>
2023-04-09 03:58:18 +0900

    mmm

--- src/components/main/sub.jsx
+++ src/components/main/sub.jsx
@@ -2,10 +2,12 @@ import { BigLabel } from "../ui/label";
 import SubCard from "./subcard";
 const Sub = () => {
   return (
-    <div>
-      <BigLabel></BigLabel>
-      <h1>цены на подписку</h1>
-      <div>
+    <div className="flex flex-col items-start">
+      <BigLabel>подписка</BigLabel>
+      <h1 className="font-medium text-[96px] mt-6 leading-[116px]">
+        <span className="text-blue-500">цены</span> на подписку
+      </h1>
+      <div className="mt-7">
         <SubCard title="Стандарт" price="999" level={1}>
           После прохождения сможете самостоятельно выполнять заказы на фрилансе
           или устроиться в агентство
